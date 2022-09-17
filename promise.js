57e3c470bbe545169804cfbf94a1ccd7713de788
Kintanr <moleny12@gmail.com>
2022-09-17 20:11:40 +0700

    add function promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (item) => {
+    try{
+      const theaterIXX = await promiseTheaterIXX()
+      const theaterVGC = await promiseTheaterVGC()
+      const allData = [...theaterIXX,...theaterVGC]
+      const filterData = allData.filter((x) => x.hasil == item)
+      return filterData.length
+    }catch(e){
+      return console.log(e)
+    } 
+}
 
 module.exports = {
   promiseOutput,
